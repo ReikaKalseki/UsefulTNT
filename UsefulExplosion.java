4fed0078a265a2bc4c86c30ebe155361e468a087
Reika <reikasminecraft@gmail.com>
2013-09-17 21:13:42 -0400

    Creation

---
+++ UsefulExplosion.java
@@ -0,0 +1,22 @@
+/*******************************************************************************
+ * @author Reika Kalseki
+ * 
+ * Copyright 2013
+ * 
+ * All rights reserved.
+ * Distribution of the software in any form is only allowed with
+ * explicit, prior permission from the owner.
+ ******************************************************************************/
+package Reika.UsefulTNT;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.world.Explosion;
+import net.minecraft.world.World;
+
+public class UsefulExplosion extends Explosion {
+
+	public UsefulExplosion(World par1World, Entity par2Entity, double par3, double par5, double par7, float par9) {
+		super(par1World, par2Entity, par3, par5, par7, par9);
+	}
+
+}
---
+++ UsefulTNT.java
@@ -0,0 +1,87 @@
+/*******************************************************************************
+ * @author Reika Kalseki
+ * 
+ * Copyright 2013
+ * 
+ * All rights reserved.
+ * Distribution of the software in any form is only allowed with
+ * explicit, prior permission from the owner.
+ ******************************************************************************/
+package Reika.UsefulTNT;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import Reika.DragonAPI.Base.DragonAPIMod;
+import Reika.DragonAPI.Exception.RegistrationException;
+import cpw.mods.fml.common.Mod.Init;
+import cpw.mods.fml.common.Mod.PostInit;
+import cpw.mods.fml.common.Mod.PreInit;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+public class UsefulTNT extends DragonAPIMod {
+
+	//@Instance
+	public static UsefulTNT instance = new UsefulTNT();
+
+	@Override
+	@PreInit
+	public void preload(FMLPreInitializationEvent evt) {
+
+	}
+
+	@Override
+	@Init
+	public void load(FMLInitializationEvent event) {
+
+	}
+
+	@Override
+	@PostInit
+	public void postload(FMLPostInitializationEvent evt) {
+
+	}
+
+	@Override
+	public String getDisplayName() {
+		return "Useful TNT";
+	}
+
+	@Override
+	public String getModAuthorName() {
+		return "Reika";
+	}
+
+	@Override
+	public URL getDocumentationSite() {
+		try {
+			return new URL("http://www.minecraftforum.net/topic/1969694-");
+		}
+		catch (MalformedURLException e) {
+			throw new RegistrationException(instance, "The mod provided a malformed URL for its documentation site!");
+		}
+	}
+
+	@Override
+	public boolean hasWiki() {
+		return false;
+	}
+
+	@Override
+	public URL getWiki() {
+		return null;
+	}
+
+	@Override
+	public boolean hasVersion() {
+		return false;
+	}
+
+	@Override
+	public String getVersionName() {
+		return null;
+	}
+
+}
