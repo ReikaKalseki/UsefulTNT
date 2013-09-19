ec8ba76f12fe8f6348a80eb9d2b5cd3771cc8fc7
Reika <reikasminecraft@gmail.com>
2013-09-19 00:51:09 -0400

    Update

--- UsefulTNT.java
+++ UsefulTNT.java
@@ -14,13 +14,17 @@ import java.net.URL;
 
 import Reika.DragonAPI.Base.DragonAPIMod;
 import Reika.DragonAPI.Exception.RegistrationException;
+import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.Init;
 import cpw.mods.fml.common.Mod.PostInit;
 import cpw.mods.fml.common.Mod.PreInit;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.network.NetworkMod;
 
+@Mod( modid = "UsefulTNT", name="Useful TNT", version="beta", certificateFingerprint = "@GET_FINGERPRINT@", dependencies="after:DragonAPI")
+@NetworkMod(clientSideRequired = true, serverSideRequired = true)
 public class UsefulTNT extends DragonAPIMod {
 
 	//@Instance
