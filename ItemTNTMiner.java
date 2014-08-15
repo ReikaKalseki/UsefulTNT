661d25ed06c4a7b72c958b20f91e0eb7d435fbe4
Reika <reikasminecraft@gmail.com>
2014-08-15 15:57:37 -0400

    1.7 Update

--- ItemTNTMiner.java
+++ ItemTNTMiner.java
@@ -9,30 +9,32 @@
  ******************************************************************************/
 package Reika.UsefulTNT;
 
+import Reika.DragonAPI.Instantiable.MiningExplosion;
+
 import net.minecraft.block.Block;
-import net.minecraft.client.renderer.texture.IconRegister;
+import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.Icon;
+import net.minecraft.util.IIcon;
 import net.minecraft.world.World;
-import Reika.DragonAPI.Instantiable.MiningExplosion;
 
 public class ItemTNTMiner extends Item {
 
-	public ItemTNTMiner(int ID) {
-		super(ID);
+	public ItemTNTMiner() {
+		super();
 		this.setMaxStackSize(1);
 		this.setMaxDamage(0);
 		this.setCreativeTab(CreativeTabs.tabTools);
 	}
 
 	@Override
-	public boolean onItemUse(ItemStack is, EntityPlayer ep, World world, int x, int y, int z, int s, float a, float b, float c) {
-		int id = world.getBlockId(x, y, z);
-		if (id == Block.tnt.blockID) {
-			world.setBlock(x, y, z, 0);
+	public boolean onItemUse(ItemStack is, EntityPlayer ep, World world, int x, int y, int z, int s, float a0, float a1, float a2) {
+		Block b = world.getBlock(x, y, z);
+		if (b == Blocks.tnt) {
+			world.setBlock(x, y, z, Blocks.air);
 			MiningExplosion exp = new MiningExplosion(world, null, x+0.5, y+0.5, z+0.5, 4, !ep.isSneaking());
 			exp.doExplosionA();
 			exp.doExplosionB(true);
@@ -42,12 +44,12 @@ public class ItemTNTMiner extends Item {
 	}
 
 	@Override
-	public void registerIcons(IconRegister ico) {
+	public void registerIcons(IIconRegister ico) {
 		itemIcon = ico.registerIcon("UsefulTNT:tool");
 	}
 
 	@Override
-	public Icon getIconFromDamage(int dmg) {
+	public IIcon getIconFromDamage(int dmg) {
 		return itemIcon;
 	}
 
--- UsefulTNT.java
+++ UsefulTNT.java
@@ -9,28 +9,27 @@
  ******************************************************************************/
 package Reika.UsefulTNT;
 
-import java.net.URL;
-
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
 import Reika.DragonAPI.DragonAPICore;
 import Reika.DragonAPI.Auxiliary.CommandableUpdateChecker;
 import Reika.DragonAPI.Base.DragonAPIMod;
-import Reika.DragonAPI.Exception.IDConflictException;
 import Reika.DragonAPI.Instantiable.IO.ModLogger;
-import Reika.DragonAPI.Instantiable.IO.SimpleConfig;
+
+import java.net.URL;
+
+import net.minecraft.init.Items;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.Mod.Instance;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
-import cpw.mods.fml.common.network.NetworkMod;
 import cpw.mods.fml.common.registry.GameRegistry;
 import cpw.mods.fml.common.registry.LanguageRegistry;
 
 @Mod( modid = "UsefulTNT", name="Useful TNT", version="beta", certificateFingerprint = "@GET_FINGERPRINT@", dependencies="required-after:DragonAPI")
-@NetworkMod(clientSideRequired = true, serverSideRequired = true)
+
 public class UsefulTNT extends DragonAPIMod {
 
 	@Instance("UsefulTNT")
@@ -40,19 +39,19 @@ public class UsefulTNT extends DragonAPIMod {
 
 	public static ModLogger logger;
 
-	public static final SimpleConfig config = new SimpleConfig(instance);
+	//public static final SimpleConfig config = new SimpleConfig(instance);
 
 	@Override
 	@EventHandler
 	public void preload(FMLPreInitializationEvent evt) {
-		config.loadSubfolderedConfigFile(evt);
-		config.loadDataFromFile(evt);
-		int itemID = config.getInteger("Item IDs", "TNT Miner Item", 27600);
-		config.finishReading();
+		//config.loadSubfolderedConfigFile(evt);
+		//config.loadDataFromFile(evt);
+		//int itemID = config.getInteger("Item IDs", "TNT Miner Item", 27600);
+		//config.finishReading();
 
-		if (Item.itemsList[256+itemID] != null)
-			throw new IDConflictException(UsefulTNT.instance, itemID+" item slot already occupied by "+Item.itemsList[256+itemID]+" while adding "+this);
-		tntItem = new ItemTNTMiner(itemID).setUnlocalizedName("tntminer");
+		//if (Items.itemsList[256+itemID] != null)
+		//	throw new IDConflictException(UsefulTNT.instance, itemID+" item slot already occupied by "+Items.itemsList[256+itemID]+" while adding "+this);
+		tntItem = new ItemTNTMiner().setUnlocalizedName("tntminer");
 		logger = new ModLogger(instance, false);
 		LanguageRegistry.addName(tntItem, "TNT Mining Item");
 
@@ -66,7 +65,7 @@ public class UsefulTNT extends DragonAPIMod {
 	}
 
 	public static void addRecipes() {
-		GameRegistry.addShapedRecipe(new ItemStack(tntItem.itemID, 1, 0), "IDI", " E ", " I ", 'I', Item.ingotIron, 'D', Item.diamond, 'E', Item.enderPearl);
+		GameRegistry.addShapedRecipe(new ItemStack(tntItem, 1, 0), "IDI", " E ", " I ", 'I', Items.iron_ingot, 'D', Items.diamond, 'E', Items.ender_pearl);
 	}
 
 	@Override
